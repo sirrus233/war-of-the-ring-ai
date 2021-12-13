40c01d5efbeb787e0660d2ecead8b25957bf9d3b
Bradley Sherman <sirrus233@gmail.com>
2021-12-13 05:42:10 -0800

    Starter enums and dataclasses

---
+++ war_of_the_ring_ai/game_objects.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+
+class Nation(Enum):
+    ELVES = 0
+    DWARVES = 1
+    NORTH = 2
+    ROHAN = 3
+    GONDOR = 4
+    SAURON = 5
+    ISENGARD = 6
+    SOUTHRON = 7
+
+
+class Settlement(Enum):
+    TOWN = 0
+    CITY = 1
+    STRONGHOLD = 2
+    FORTIFICATION = 3
+
+
+class CharacterID(Enum):
+    GANDALF_GREY = 0
+    STRIDER = 1
+    GIMLI = 2
+    LEGOLAS = 3
+    BOROMIR = 4
+    MERRY = 5
+    PIPPIN = 6
+    GANDALF_WHITE = 7
+    ARAGORN = 8
+    SARUMAN = 9
+    WITCH_KING = 10
+    MOUTH_OF_SAURON = 11
+
+
+@dataclass
+class Region:
+    name: str
+    neighbors: list["Region"]
+    nation: Optional[Nation] = None
+    settlement: Optional[Settlement] = None
+
+
+@dataclass
+class Character:
+    name: CharacterID
+    level: int
+    leadership: int
+
+
+@dataclass
+class Companion(Character):
+    pass
+
+
+@dataclass
+class Minion(Character):
+    pass
+
+
+@dataclass
+class Fellowship:
+    guide: Companion
+    progress: int = 0
+    corruption: int = 0
